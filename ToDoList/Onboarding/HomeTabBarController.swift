e66642307e5713cfa8af48fcecc6943746ab0dab
xyw0025 <xyw0025@gmail.com>
2020-06-27 17:10:15 +0800

    Feat: Task screen show all tasks

--- ToDoList/Onboarding/HomeTabBarController.swift
+++ ToDoList/Onboarding/HomeTabBarController.swift
@@ -12,4 +12,27 @@ class HomeTabBarController: UITabBarController {
     func changeSelectedIndex(to index: Int) {
         selectedIndex = index
     }
+
+    
+
+    var taskArchive = TaskArchive()
+
+
+
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("prepare for calendar")
+
+        let controller = segue.destination as? UITabBarController
+        let calendar = controller?.viewControllers?[2] as! CalendarViewController
+        calendar.checkForUpdates()
+//        calendar.getDataFromFirebase()
+        calendar.calendar.reloadData()
+
+
+
+    }
 }
+
+
+
--- ToDoList/Task/ViewController.swift
+++ ToDoList/Task/ViewController.swift
@@ -7,37 +7,98 @@
 //
 import SideMenu
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     var menu: SideMenuNavigationController?
+    var taskArchive = TaskArchive()
+    let db = Firestore.firestore()
 
 
     var button = AddTaskButton()
       override func viewDidLoad() {
         super.viewDidLoad()
         if isDarkMode() {print("isDarkMode")} else {
-            print("isntDarkMode")
+//            print("isntDarkMode")
         }
-
-        navigationController?.hideNavigationItemBackground()
-        view.backgroundColor = .white
-        self.navigationController?.isNavigationBarHidden = true
-        menu = SideMenuNavigationController(rootViewController: MenuListController())
-        menu?.leftSide = true
-        menu?.setNavigationBarHidden(true, animated: false)
-        SideMenuManager.default.leftMenuNavigationController = menu
-        menu?.statusBarEndAlpha = 0
-        SideMenuManager.default.addPanGestureToPresent(toView: self.view)
-
+        renderSideMenu()
         button.configureButton(to: view)
-
+        checkForUpdates()
+        configureTableView()
 
       }
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 80
+        tableView.register(DailyTaskCell.self, forCellReuseIdentifier: "DailyTaskCell")
+    }
+    func getDataFromFirebase() {
+        db.collection("Tasks").order(by: "date").getDocuments() {
+            querySnapshot, error in
+            
+            self.taskArchive.tasks = []
+            if let querySnapshot = querySnapshot {
+                for task in querySnapshot.documents {
+                    let item = Task(id: task.documentID ,title: task.data()["title"] as! String, content: task.data()["content"] as! String, date: task.data()["date"] as! String, status: task.data()["status"] as! Bool, tags: task.data()["tags"] as! [String])
+                    self.taskArchive.tasks += [item]
+                }
+                self.taskArchive.getAllDates()
+                self.tableView.reloadData()
+                
+                
+            }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func checkForUpdates() {
+        db.collection("Tasks").order(by: "date").addSnapshotListener {
+            querySnapshot, error in
+            self.getDataFromFirebase()
+        }
+    }
+    
+    
+    
+    
 
 
 
+    override func viewWillAppear(_ animated: Bool) {
+        let tabBar = tabBarController as! HomeTabBarController
+        taskArchive = tabBar.taskArchive
+    }
 
+    
+    func renderSideMenu() {
+        menu = SideMenuNavigationController(rootViewController: MenuListController())
+        menu?.leftSide = false
+
+        SideMenuManager.default.leftMenuNavigationController = menu
+        menu?.statusBarEndAlpha = 0
+        SideMenuManager.default.addScreenEdgePanGesturesToPresent(toView: view)
+
+    }
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskArchive.tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DailyTaskCell") as! DailyTaskCell
+        cell.selectionStyle = .none
+        let task = taskArchive.tasks[indexPath.row]
+        cell.setTask(to: task)
+        return cell
+    }
 }
 
 extension UIViewController {
